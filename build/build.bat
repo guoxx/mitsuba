7f80a9839bb6bd63878901f764e7781a10e22aec
guoxiaoxin <guoxx@me.com>
2017-12-10 22:35:24 +0800

    Fix compiling errors for windows.

---
+++ build/build.bat
@@ -0,0 +1,43 @@
+@::!/dos/rocks
+@echo off
+goto :parse
+
+:parse
+if "%~1"=="" goto :end
+
+    if /i "%~1"=="/debug"       set "OptDebug=yes"  & shift & goto :parse
+    if /i "%~1"=="-debug"       set "OptDebug=yes"  & shift & goto :parse
+    if /i "%~1"=="--debug"      set "OptDebug=yes"  & shift & goto :parse
+
+    if /i "%~1"=="/build"       goto :build
+    if /i "%~1"=="-build"       goto :build
+    if /i "%~1"=="--build"      goto :build
+    
+    if /i "%~1"=="/clean"       goto :clean
+    if /i "%~1"=="-clean"       goto :clean
+    if /i "%~1"=="--clean"      goto :clean
+
+    shift
+
+:build
+    call C:\Users\ben\Anaconda3\Scripts\activate.bat mitsuba_build
+    cd ..
+    if defined OptDebug (
+        scons --parallelize --cfg=build\config-win64-msvc2017-debug.py
+    ) else (
+        scons --parallelize --cfg=build\config-win64-msvc2017.py
+    )
+    goto :end
+
+:clean
+    call C:\Users\ben\Anaconda3\Scripts\activate.bat mitsuba_build
+    cd ..
+    if defined OptDebug (
+        scons --parallelize --cfg=build\config-win64-msvc2017-debug.py -c
+    ) else (
+        scons --parallelize --cfg=build\config-win64-msvc2017.py -c
+    )
+    goto :end
+
+:end
+    exit /B
